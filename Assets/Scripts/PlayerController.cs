a348347c2ad6d309d4ded948d51d5278f69f5dd6
Mohamed ElHariri <mohamed-elhariri@hotmail.com>
2023-11-21 19:52:22 +0200

    Implemented Player Idle and Running Animations

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -7,13 +8,17 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float moveSpeed = 1f;
 
     private PlayerControls playerControls;
-    private Vector2 movement;
+    private UnityEngine.Vector2 movement;
     private Rigidbody2D rb;
+    private Animator myAnimator;
+    private SpriteRenderer mySpriteRenderer;
 
     private void Awake() 
     {
         playerControls = new PlayerControls();
         rb = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+        mySpriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     private void OnEnable() 
@@ -28,18 +33,37 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate() 
     {
+        AdjustPlayerFacingDirection();
         Move();
     }
 
     private void PlayerInput()
     {
-        movement = playerControls.Movement.Move.ReadValue<Vector2>();
+        movement = playerControls.Movement.Move.ReadValue<UnityEngine.Vector2>();
+
+        myAnimator.SetFloat("moveX", movement.x);
+        myAnimator.SetFloat("moveY", movement.y);
         //Debug.Log(movement);
     }
 
     private void Move()
     {
         rb.MovePosition(rb.position + movement * (moveSpeed * Time.fixedDeltaTime));
+    }
+
+    private void AdjustPlayerFacingDirection()
+    {
+        UnityEngine.Vector3 mousePos = Input.mousePosition;
+        UnityEngine.Vector3 playerScreenPoint = Camera.main.WorldToScreenPoint(transform.position);
+
+        if (mousePos.x < playerScreenPoint.x)
+        {
+            mySpriteRenderer.flipX = true;
+        }
+        else
+        {
+            mySpriteRenderer.flipX = false;
+        }
     }
 
 }
