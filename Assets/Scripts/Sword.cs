0982d92e85e2d475d71e1fdb1b5c5d5223cde373
Mohamed ElHariri <mohamed-elhariri@hotmail.com>
2023-11-22 19:15:57 +0200

    Added Code to allow swinging the sword in both directions

--- Assets/Scripts/Sword.cs
+++ Assets/Scripts/Sword.cs
@@ -1,16 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
 using UnityEngine;
 
 public class Sword : MonoBehaviour
 {
     private PlayerControls playerControls;
     private Animator myAnimator;
+    private PlayerController playerController;
+    private ActiveWeapon activeWeapon;
 
     private void Awake() 
     {
-        playerControls = new PlayerControls();
+        playerController = GetComponentInParent<PlayerController>();
+        activeWeapon = GetComponentInParent<ActiveWeapon>();
         myAnimator = GetComponent<Animator>();
+        playerControls = new PlayerControls();
     }
 
     private void OnEnable() {
@@ -22,9 +27,31 @@ public class Sword : MonoBehaviour
         playerControls.Combat.Attack.started += _ => Attack();
     }
 
+    private void Update() 
+    {
+        MouseFollowWithOffset();
+    }
+
     private void Attack()
     {
         myAnimator.SetTrigger("Attack");
+    }
+
+    private void MouseFollowWithOffset()
+    {
+        UnityEngine.Vector3 mousePos = Input.mousePosition;
+        UnityEngine.Vector3 playerScreenPoint = Camera.main.WorldToScreenPoint(playerController.transform.position);
+
+        float angle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
+
+        if(mousePos.x < playerScreenPoint.x)
+        {
+            activeWeapon.transform.rotation = UnityEngine.Quaternion.Euler(0, -180, angle);
+        }
+        else
+        {
+            activeWeapon.transform.rotation = UnityEngine.Quaternion.Euler(0, 0, angle);
+        }
     }
 
     
